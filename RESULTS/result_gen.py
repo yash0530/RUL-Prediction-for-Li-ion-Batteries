4031c8a7afeec4f65429da6bec316c5efc0a9bc9
Yash Jain <yashjain0530@gmail.com>
2021-01-18 16:51:02 +0530

    added results (pso remaining)

---
+++ RESULTS/result_gen.py
@@ -0,0 +1,61 @@
+locs = {
+    "/home/yash/Documents/RUL/RESULTS/ARD Reg": "ARD Reg",
+    "/home/yash/Documents/RUL/RESULTS/Decision Tree Reg": "Decision Tree Reg",
+    "/home/yash/Documents/RUL/RESULTS/ELM Reg": "ELM Reg",
+    "/home/yash/Documents/RUL/RESULTS/Linear Reg": "Linear Reg",
+    "/home/yash/Documents/RUL/RESULTS/Naive Bayes Reg": "Naive Bayes Reg",
+    "/home/yash/Documents/RUL/RESULTS/Random Forest Reg": "Random Forest Reg"
+}
+
+ranges = [
+    "1-20", "21-50", "51-70", "71-90"
+]
+
+# ranges = [
+#     "21-50"
+# ]
+
+results = {}
+
+from pprint import pprint as pp
+
+for ran in ranges:
+    curr_result = [["Battery"], ["B0005"], ["B0006"], ["B0007"], ["B0018"]]
+
+    for loc in locs:
+        curr_result[0].append(locs[loc])
+        location = f"{loc}/{ran}/result.txt"
+        with open(location,'r') as f:
+            lines = f.readlines()
+
+            j = 1
+            for i in range(0, len(lines), 4):
+                battery = lines[i][:-1]
+                test_minima = lines[i + 1].split()[2][:-10]
+                test_param = lines[i + 1].split()[5]
+                train_minima = lines[i + 2].split()[2][:-10]
+                train_param = lines[i + 2].split()[5]
+                curr_result[j].append(f"{test_minima} ({test_param})")
+                j += 1
+    results[ran] = curr_result
+
+for ran in ranges:
+    for i in range(1, 5):
+        row = results[ran][i][1:]
+        mn = 1000
+        loc = -1
+        for j in range(len(row)):
+            if (mn > float(row[j].split()[0])):
+                mn = float(row[j].split()[0])
+                loc = j
+        results[ran][i][loc + 1] = f"** {results[ran][i][loc + 1]}"
+
+from prettytable import PrettyTable
+for ran in ranges:
+    p = PrettyTable()
+    p.title = f"Merge Param Range: {ran}"
+    p.field_names = results[ran][0]
+    for row in results[ran][1:]:
+        p.add_row(row)
+    print(p)
+    print("\n")
