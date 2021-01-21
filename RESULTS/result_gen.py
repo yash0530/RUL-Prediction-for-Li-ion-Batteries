a5c7dac8893263330a1fb7950f71847d9df4cd47
Yash Jain <yashjain0530@gmail.com>
2021-01-21 23:23:45 +0530

    ..

--- RESULTS/result_gen.py
+++ RESULTS/result_gen.py
@@ -28,9 +28,9 @@ for ran in ranges:
             j = 1
             for i in range(0, len(lines), 4):
                 battery = lines[i][:-1]
-                test_minima = lines[i + 1].split()[2][:-10]
+                test_minima = lines[i + 1].split()[2][:-12]
                 test_param = lines[i + 1].split()[5]
-                train_minima = lines[i + 2].split()[2][:-10]
+                train_minima = lines[i + 2].split()[2][:-12]
                 train_param = lines[i + 2].split()[5]
                 curr_result[j].append(f"{test_minima} ({test_param})")
                 j += 1
@@ -53,6 +53,32 @@ for ran in ranges:
     p.title = f"Merge Param Range: {ran}"
     p.field_names = results[ran][0]
     for row in results[ran][1:]:
+        p.add_row(row)
+    print(p)
+    print("\n")
+
+
+results_accuracy = results
+for ran in ranges:
+    for i in range(1, 5):
+        for j in range(len(results[ran][i][1:])):
+            if (results[ran][i][j + 1]):
+                if (results[ran][i][j + 1][0] == '*'):
+                    merge = results[ran][i][j + 1].split(' ')[2]
+                    abc = results[ran][i][j + 1].split(' ')[1]
+                    acc = (1 - float(abc)) * 100
+                    results_accuracy[ran][i][j + 1] = f"** {round(acc, 5)} {merge}"
+                else:
+                    abc = results[ran][i][j + 1].split(' ')[0]
+                    merge = results[ran][i][j + 1].split(' ')[1]
+                    acc = (1 - float(abc)) * 100
+                    results_accuracy[ran][i][j + 1] = f"{round(acc, 5)} {merge}"
+
+for ran in ranges:
+    p = PrettyTable()
+    p.title = f"Merge Param Range: {ran}"
+    p.field_names = results_accuracy[ran][0]
+    for row in results_accuracy[ran][1:]:
         p.add_row(row)
     print(p)
     print("\n")
